f0dc1522d5a1002fd47c95fef40e395fb5074ae3
guy7cc <alrdy.tree1sawa@gmail.com>
2023-12-27 06:07:25 +0900

    Formatted scripts

--- LCNoPropsLost/Patches/RoundManagerPatch.cs
+++ LCNoPropsLost/Patches/RoundManagerPatch.cs
@@ -1,53 +1,50 @@
 ﻿using HarmonyLib;
-using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Reflection.Emit;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace LCNoPropsLost.Patches
 {
-	[HarmonyPatch(typeof(RoundManager))]
-	class RoundManagerPatch
+    [HarmonyPatch(typeof(RoundManager))]
+    class RoundManagerPatch
     {
-		[HarmonyPatch("DespawnPropsAtEndOfRound")]
-		[HarmonyTranspiler]
-		static IEnumerable<CodeInstruction> keepProps(IEnumerable<CodeInstruction> instructions, ILGenerator il)
-		{
-			var code = new List<CodeInstruction>(instructions);
-			int insertionIndex = -1;
-			Label label = il.DefineLabel();
+        [HarmonyPatch("DespawnPropsAtEndOfRound")]
+        [HarmonyTranspiler]
+        static IEnumerable<CodeInstruction> keepProps(IEnumerable<CodeInstruction> instructions, ILGenerator il)
+        {
+            var code = new List<CodeInstruction>(instructions);
 
-			var type = typeof(StartOfRound);
-			var property = type.GetProperty("Instance");
-			var getter = property.GetMethod;
-			var field = type.GetField("allPlayersDead");
+            int insertionIndex = -1;
+            Label label = il.DefineLabel();
 
-			object target = null;
+            var type = typeof(StartOfRound);
+            var property = type.GetProperty("Instance");
+            var getter = property.GetMethod;
+            var field = type.GetField("allPlayersDead");
 
-			for (int i = 0; i < code.Count - 2; i++)
-			{
-				if (code[i].opcode == OpCodes.Call 
-					&& code[i].Calls(getter) 
-					&& code[i + 1].opcode == OpCodes.Ldfld 
-					&& code[i + 1].LoadsField(field)
-					&& code[i + 2].opcode == OpCodes.Brfalse)
+            int i = 0;
+            for (; i < code.Count - 2; i++)
+            {
+                LCNoPropsLostPlugin.Logger.LogInfo(i + code[i].opcode.ToString());
+                if (code[i].opcode == OpCodes.Call
+                    && code[i].Calls(getter)
+                    && code[i + 1].opcode == OpCodes.Ldfld
+                    && code[i + 1].LoadsField(field)
+                    && code[i + 2].opcode == OpCodes.Brfalse)
                 {
-					insertionIndex = i;
-					code[i].labels.Add(label);
-					target = code[i + 2].operand;
-					break;
-				}
-			}
-			var instructionsToInsert = new List<CodeInstruction>();
-			instructionsToInsert.Add(new CodeInstruction(OpCodes.Br, target));
+                    insertionIndex = i;
+                    label = (Label)code[i + 2].operand;
+                    break;
+                }
+            }
 
-			if (insertionIndex != -1)
-			{
-				code.InsertRange(insertionIndex, instructionsToInsert);
-			}
-			return code;
-		}
-	}
+            if (insertionIndex != -1)
+            {
+                var instructionsToInsert = new List<CodeInstruction>();
+                instructionsToInsert.Add(new CodeInstruction(OpCodes.Br, label));
+                code.InsertRange(insertionIndex, instructionsToInsert);
+            }
+
+            return code;
+        }
+    }
 }
--- LCNoPropsLost/Plugin.cs
+++ LCNoPropsLost/Plugin.cs
@@ -2,11 +2,6 @@
 using BepInEx.Logging;
 using HarmonyLib;
 using LCNoPropsLost.Patches;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace LCNoPropsLost
 {
